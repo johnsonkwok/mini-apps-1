b7e48a2b41a03a93adfcee5ae23c9963e3ee6d69
Johnson Kwok <johnsonkwok1@gmail.com>
2018-12-12 12:40:59 -0800

    Challenge 3: Set up index.html and render checkout button

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,23 @@
 
+const checkout = () => (
+  <FormF1 />
+);
+
+const Button = (props) => (
+  <button type="button" id="checkout" onClick={checkout}>Checkout</button>
+);
+
+class FormF1 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h2>works!</h2>
+    );
+  }
+}
+
+ReactDOM.render(<Button />, document.getElementById('home'));
 
