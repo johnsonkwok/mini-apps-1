a7615b3d12fda49dac4d07fb9f69d4c3855dd3d9
Johnson Kwok <johnsonkwok1@gmail.com>
2018-12-12 14:02:31 -0800

    Challenge 3: Make Button a stateful class component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,11 +1,24 @@
 
-const checkout = () => (
-  <FormF1 />
-);
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayButton = this.displayButton.bind(this);
+    this.state = {
+      button: (<button type="button" id="checkout" onClick={this.displayButton}>Checkout</button>),
+      viewBtn: true
+    };
+  }
 
-const Button = (props) => (
-  <button type="button" id="checkout" onClick={checkout}>Checkout</button>
-);
+  displayButton() {
+    this.setState({
+      viewBtn: !this.state.viewBtn
+    });
+  }
+
+  render() {
+    return (this.state.viewBtn ? this.state.button : <FormF1 />);
+  }
+}
 
 class FormF1 extends React.Component {
   constructor(props) {
@@ -14,10 +27,36 @@ class FormF1 extends React.Component {
 
   render() {
     return (
-      <h2>works!</h2>
+      <form action=""></form>
     );
   }
 }
+
+class FormF2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form action=""></form>
+    );
+  }
+}
+
+class FormF3 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form action=""></form>
+    );
+  }
+}
+
+
 
 ReactDOM.render(<Button />, document.getElementById('home'));
 
