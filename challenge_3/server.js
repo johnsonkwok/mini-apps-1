974b52a1cf2f0c28f9ffdd987885c2cadf79c611
Johnson Kwok <johnsonkwok1@gmail.com>
2018-12-12 14:21:25 -0800

    Challenge 3: Initialize server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(express.static('./public'));
+app.listen(port, () => {console.log(`App listening on port ${port}`)});
+
+app.get('/', (req, res) => {
+  console.log('cool');
+});
+
+
 
 
