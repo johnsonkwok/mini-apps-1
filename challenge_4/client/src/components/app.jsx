9c643e3ff66bb2a944f0fe2f8bae0bc0f63cff37
Johnson Kwok <johnsonkwok1@gmail.com>
2018-12-13 21:43:22 -0800

    Challenge 4: Detect end of game and display winner/tie

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -18,7 +18,6 @@ class App extends React.Component {
   }
 
   updateBoard(x, y) {
-    // let cell = document.querySelector(`[data-x='${x}'][data-y='${y}']`);
     let move = this.state.currPlayer ? 1 : 2;
     let currRow = 5;
     while (this.state[currRow][y] !== 0 && currRow > 0) {
@@ -36,26 +35,120 @@ class App extends React.Component {
   }
 
   detectGameEnd() {
-    if (detectHorizWin() || detectVertWin() || detectDiagWin()) {
-      
-    }
+    let winner = 'TIE!';
     
-    function detectHorizWin() {
-
+    const detectHorizWin = () => {
+      let gameOver = false;
+      for (let i = 5; i >= 0; i--) {
+        let pieces = '';
+        if (!gameOver) {
+          for (let j = 0; j < this.state[i].length; j++) {
+            let currPosition = this.state[i][j];
+            pieces += currPosition;
+          }
+          if (pieces.includes('1111')) {
+            gameOver = true;
+            winner = 'PLAYER 1 (RED)';
+          } else if (pieces.includes('2222')) {
+            gameOver = true;
+            winner = 'PLAYER 2 (YELLOW)';
+          }
+        }
+      }
+      return gameOver;
+    }  
+    
+    const detectVertWin = () => {
+      let gameOver = false;
+      for (let i = 6; i >= 0; i--) {
+        let pieces = '';
+        if (!gameOver) {
+          for (let j = 0; j < 6; j++) {
+            let currPosition = this.state[j][i];
+            pieces += currPosition;
+          }
+          if (pieces.includes('1111')) {
+            gameOver = true;
+            winner = 'PLAYER 1 (RED)';
+          } else if (pieces.includes('2222')) {
+            gameOver = true;
+            winner = 'PLAYER 2 (YELLOW)';
+          }
+        }
+      }
+      return gameOver;
+    };
+    
+    const detectMajorDiagWin = () => {
+      let gameOver = false;
+      for (let i = -2; i <= 3; i++) {
+        let pieces = '';
+        if (!gameOver) {
+          let c = i;
+          let r = 0;
+          while (r <= 5 && c <= 6) {
+            let currPosition = this.state[r][c];
+            pieces += currPosition || '0';
+            c++;
+            r++;
+          }
+          if (pieces.includes('1111')) {
+            gameOver = true;
+            winner = 'PLAYER 1 (RED)';
+          } else if (pieces.includes('2222')) {
+            gameOver = true;
+            winner = 'PLAYER 2 (YELLOW)';
+          }
+        }
+      }
+      return gameOver;
     }
 
-    function detectVertWin() {
+    const detectMinorDiagWin = () => {
+      let gameOver = false;
+      for (let i = 8; i >= 0; i--) {
+        let pieces = '';
+        if (!gameOver) {
+          let c = i;
+          let r = 0;
+          while (r <= 5 && c >= 0) {
+            let currPosition = this.state[r][c];
+            pieces += currPosition || '0';
+            c--;
+            r++;
+          }
+          if (pieces.includes('1111')) {
+            gameOver = true;
+            winner = 'PLAYER 1 (RED)';
+          } else if (pieces.includes('2222')) {
+            gameOver = true;
+            winner = 'PLAYER 2 (YELLOW)';
+          }
+        }
+      }
+      return gameOver;
+    };
 
-    }
-
-    function detectDiagWin() {
-
+    if (detectHorizWin() || detectVertWin() || detectMajorDiagWin() || detectMinorDiagWin()) {
+      return winner;
+    } else {
+      return false;
     }
   }
 
   render() {
+    let page = null;
+    let winner = this.detectGameEnd();
+    if (!!winner) {
+      page = (<div><h1>{`${winner} WINS!`}</h1></div>);
+    } else if (this.state['0'].every(y => (y !== 0))) {
+      page = (<div><h1>{'TIE!'}</h1></div>);
+    }
     return(
-      <Board updBoard={this.updateBoard} state={this.state} onChange={this.detectGameEnd} />
+      <div>
+        <Board updBoard={this.updateBoard} state={this.state} />
+        {page}
+      </div>
     );
   }
 }
